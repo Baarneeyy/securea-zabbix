47defb0ddf2a3d0c0071dabbe539232e7f113b02
Tomas Vsetecka <tvsetecka@csoc.eu>
2024-11-21 09:09:46 +0100

    investigate if prod access given for zabbix;

--- cypress/e2e/zabbix/secureaPROD.cy.js
+++ cypress/e2e/zabbix/secureaPROD.cy.js
@@ -1,10 +1,10 @@
 describe('checks if able to get into app.securea', () => {
     it('logs in', () => {
         //Setup + Asset Browser
-        cy.setupUser(Cypress.env('PRE_USER'), Cypress.env('PRE_PASS'), 'TVsetecka test tenant', 'Management', 'Risk Management', 'Asset Browser')
+        cy.visit(Cypress.env('url'))
+        cy.wait(1500)
         cy.url().then((url) => {
-            cy.wrap(url).should('contain', 'asset-browser')
+            cy.log(url)
         })
-        cy.wait(250)
     })
 })
