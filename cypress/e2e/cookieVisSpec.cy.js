ae6d24de5c04766fd82c4068f1ae42806b9c92d1
LukasLubusky <lukas1.lubusky@gmail.com>
2024-12-07 16:24:34 +0100

    CookieVisSpec update

--- cypress/e2e/cookieVisSpec.cy.js
+++ cypress/e2e/cookieVisSpec.cy.js
@@ -29,7 +29,119 @@ describe("Change the Table Setting Cookies Risk Management >> Asset Browser", { 
     })
 })
 
+describe("Change the Table Setting Cookies Risk Management >> Asset Business Processes", { testIsolation: true }, () => {
+    it("Setup test user and hide Threat Count column", () => {
+        cy.setupUser('admin', 'TotoUrciteNiejeHesl0?!', 'tomas_workflow_tests', 'Management', 'Risk Management', 'Asset Browser')
+        cy.get('.wrapper__header >', {timeout:8000}).should('have.length', 4)
+        // Click the first asset
+        cy.get('.list__body > :nth-child(1) > :nth-child(2)').click()
+        // Open column visibility menu
+        cy.get('.mappings-wrapper > :nth-child(1) .sticky').click()
+        // Click the Value selector
+        cy.get(':nth-child(4) > .px-3').click()
+        // Refresh the page
+        cy.reload()
+        // Check that the Value column is not visible
+        // Open Asset Browser
+        cy.get('[data-cy="menu_management"] > .flex').click()
+        cy.wait(1000)
+        cy.get('.bg-black-200').click()
+        cy.get('.wrapper__header >', {timeout:8000}).should('have.length', 4)
+        // Click the first asset
+        cy.get('.list__body > :nth-child(1) > :nth-child(2)').click()
+        // Verify that the column 'Value' exists in the Asset Business Processes Browser
+        cy.get('.mappings-wrapper > :nth-child(1)').should('not.contain', 'Value')
+    })
+    it("Check table settings", () => {
+        cy.clearCookies()
+        cy.clearAllCookies()
+        cy.setupUser('admin', 'TotoUrciteNiejeHesl0?!', 'tomas_workflow_tests', 'Management', 'Risk Management', 'Asset Browser')
+        cy.get('.wrapper__header >', {timeout:8000}).should('have.length', 4)
+        // Click the first asset
+        cy.get('.list__body > :nth-child(1) > :nth-child(2)').click()
+        // Verify default settings restored
+        cy.get('.mappings-wrapper > :nth-child(1)').should("contain", "Value")
+    })
+})
 
+describe("Change the Table Setting Cookies Risk Management >> Asset Controls", { testIsolation: true }, () => {
+    it("Setup test user and hide Threat Count column", () => {
+        cy.setupUser('admin', 'TotoUrciteNiejeHesl0?!', 'tomas_workflow_tests', 'Management', 'Risk Management', 'Asset Browser')
+        cy.get('.wrapper__header >', {timeout:8000}).should('have.length', 4)
+        // Click the first asset
+        cy.get('.list__body > :nth-child(1) > :nth-child(2)').click()
+        // Open column visibility menu
+        cy.get('.mappings-wrapper > :nth-child(2) .sticky').click()
+        // Click the Description selector
+        cy.get(':nth-child(3) > .px-3').click()
+        // Refresh the page
+        cy.reload()
+        // Check that the Description column is not visible
+        // Open Asset Browser
+        cy.get('[data-cy="menu_management"] > .flex').click()
+        cy.wait(1000)
+        cy.get('.bg-black-200').click()
+        cy.get('.wrapper__header >', {timeout:8000}).should('have.length', 4)
+        // Click the first asset
+        cy.get('.list__body > :nth-child(1) > :nth-child(2)').click()
+        // Verify that the column 'Description' exists in the table
+        cy.get('.mappings-wrapper > :nth-child(2)').should('not.contain', 'Description')
+    })
+    it("Check table settings", () => {
+        cy.clearCookies()
+        cy.clearAllCookies()
+        cy.setupUser('admin', 'TotoUrciteNiejeHesl0?!', 'tomas_workflow_tests', 'Management', 'Risk Management', 'Asset Browser')
+        cy.get('.wrapper__header >', {timeout:8000}).should('have.length', 4)
+        // Click the first asset
+        cy.get('.list__body > :nth-child(1) > :nth-child(2)').click()
+        // Verify default settings restored
+        cy.get('.mappings-wrapper > :nth-child(2)').should("contain", "Description")
+    })
+})
+
+describe("Change the Table Setting Cookies Risk Management >> Asset Threats", { testIsolation: true }, () => {
+    it("Setup test user and hide Threat Count column", () => {
+        cy.setupUser('admin', 'TotoUrciteNiejeHesl0?!', 'tomas_workflow_tests', 'Management', 'Risk Management', 'Asset Browser')
+        cy.get('.wrapper__header >', {timeout:8000}).should('have.length', 4)
+        // Click the first asset
+        cy.get('.list__body > :nth-child(1) > :nth-child(2)').click()
+        // Open column visibility menu
+        cy.get('.mappings-wrapper > :nth-child(3) .sticky').click()
+        // Click the Probability selector
+        cy.get(':nth-child(8) > .px-3').click()
+        // Refresh the page
+        cy.reload()
+        // Check that the Probability column is not visible
+        // Open Asset Browser
+        cy.get('[data-cy="menu_management"] > .flex').click()
+        cy.wait(1000)
+        cy.get('.bg-black-200').click()
+        cy.get('.wrapper__header >', {timeout:8000}).should('have.length', 4)
+        // Click the first asset
+        cy.get('.list__body > :nth-child(1) > :nth-child(2)').click()
+        // Verify that the column 'Probability' exists in the table
+        cy.get('.mappings-wrapper > :nth-child(3)').should('not.contain', 'Probability')
+    })
+    it("Check table settings", () => {
+        cy.clearCookies()
+        cy.clearAllCookies()
+        cy.setupUser('admin', 'TotoUrciteNiejeHesl0?!', 'tomas_workflow_tests', 'Management', 'Risk Management', 'Asset Browser')
+        cy.get('.wrapper__header >', {timeout:8000}).should('have.length', 4)
+        // Click the first asset
+        cy.get('.list__body > :nth-child(1) > :nth-child(2)').click()
+        // Verify default settings restored
+        cy.get('.mappings-wrapper > :nth-child(3)').should("contain", "Probability")
+    })
+})
+
+
+
+
+
+
+
+
+/*
 describe("Change the Table Setting Cookies BCM >> Business Processes", { testIsolation: true }, () => {
     it("Setup test user and hide Asset Count column", () => {
         cy.login('admin', 'TotoUrciteNiejeHesl0?!')
@@ -142,4 +254,4 @@ describe("Change the Table Setting Risk Management >> Threat Browser", { testIso
         // Verify default settings restored
         cy.get(".wrapper").should("contain", "AC")
     })
-})
+})*/
